89d00a68d8383ba5ed1b04fc4a77e1e5d526be1d
Zach Hall <zachhallnc@gmail.com>
2020-06-10 09:03:31 -0400

    Finished BubbleSortTwo program

--- BubbleSortTwo.java
+++ BubbleSortTwo.java
@@ -7,10 +7,33 @@ public class BubbleSortTwo {
             for (int i = 0; i < lastUnsorted; i++) {
                 if (array[i] > array[i + 1]) {
                     swap(array, i, i + 1);
-                    isSorted = true;
+                    isSorted = false;
+                    printArray(array);
                 }
             }
             lastUnsorted--;
         }
+        System.out.println("-- Sorted Array --");
+        printArray(array);
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    // Prints the array
+    public static void printArray(int arr[]) {
+        // O(1) space, O(n) time
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        bubblesort(arr);
     }
 }
